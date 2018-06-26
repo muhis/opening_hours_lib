311038ad900eeaed66b6d0154caf81d747818e05
Mohammed Salman <m.salman@zinkki.com>
2018-06-26 20:43:33 +0300

    Update lib version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="opening hours parsing",
-    version='0.0.2',
+    version='0.0.3',
     description="Parse opening hours",
     author="Mohammed Salman",
     author_email="m.salman@zinkki.com",
